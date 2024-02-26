a2e4b38209e7349a6c2a1cd9b1e21872f885e98a
omar2711 <omar11santiago@hotmail.com>
2024-02-25 22:39:48 -0400

    envia mensajes socket

--- src/App.js
+++ src/App.js
@@ -1,57 +1,19 @@
 import React from 'react';
 import ServerComponent from './components/servercomponent.js';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ClientPage } from './pages/clientPage.js';
+import { server } from './pages/serverPage.js';
+import { Button } from 'react-bootstrap';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      servers: [],
-      ip: '',
-      puerto: ''
-    };
-  }
-
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  agregarServer = () => {
-    const { servers, ip, puerto } = this.state;
-    this.setState({ 
-      servers: [...servers, <ServerComponent key={servers.length} totalAlmacenamiento="100 GB" espacioLibre="30 GB" espacioOcupado="70 GB" />],
-      ip: '',
-      puerto: ''
-    });
-  }
-
-  render() {
-    const { servers, ip, puerto } = this.state;
-
-    return (
-      <div>
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
-          <Form style={{ marginBottom: '20px' }}>
-            <Form.Group controlId="ip">
-              <Form.Label>IP:</Form.Label>
-              <Form.Control type="text" name="ip" value={ip} onChange={this.handleChange} placeholder="Ingrese la dirección IP" />
-            </Form.Group>
-            <Form.Group controlId="puerto">
-              <Form.Label>Puerto:</Form.Label>
-              <Form.Control type="text" name="puerto" value={puerto} onChange={this.handleChange} placeholder="Ingrese el puerto" />
-            </Form.Group>
-            <Button onClick={this.agregarServer}>Agregar Servidor</Button>
-          </Form>
-          {servers.map((server, index) => (
-            <div key={index} style={{ marginBottom: '20px' }}>
-              {server}
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/client" element={<ClientPage/>} />
+      </Routes>
+    </BrowserRouter>
+    
+  );
 }
 
 export default App;
---
+++ src/pages/clientPage.js
@@ -0,0 +1,164 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export function ClientPage() {
+    const [isHovered, setIsHovered] = useState(false);
+
+        const buttonStyle = {
+            backgroundColor: isHovered ? '#45a049' : '#4CAF50',
+            border: 'none',
+            color: 'white',
+            padding: '15px 32px',
+            textAlign: 'center',
+            textDecoration: 'none',
+            display: 'inline-block',
+            fontSize: '16px',
+            margin: '4px 2px',
+            cursor: 'pointer',
+            borderRadius: '8px',
+            transitionDuration: '0.4s',
+            boxShadow: isHovered ? '0 4px 8px 0 rgba(0,0,0,0.2)' : 'none' 
+          
+        };
+
+        const textBoxStyle = {
+            width: '200px',
+            height: '30px',
+            padding: '5px',
+            fontSize: '16px',
+            border: '1px solid #ccc',
+            borderRadius: '4px',
+            margin: '10px',
+        };
+        
+        const centeredDivStyle = {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            height: 'auto',
+        };
+
+        const verticallyCenteredDivStyle = {
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            height: '100vh',
+        };
+          
+        const [ip, setip] = useState('');
+
+        const handleIpChange = (event) => {
+            setip(event.target.value);
+        }
+        const [port, setPort] = useState('');
+
+        const [diskInfo, setDiskInfo] = useState(null); // Estado para almacenar la información del disco
+
+
+        const handlePortChange = (event) => {
+            setPort(event.target.value);
+        }
+
+        const handleClicked = () => {
+            alert(ip + port);
+
+        }
+
+        const handleSpaceCalculation = () => {
+            
+            fetch('http://127.0.0.1:8000/api/v1/diskSpace/')
+            .then(response => response.json())
+            .then(data => {
+                // Almacenar la información del disco en el estado
+                setDiskInfo(data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert('Error al obtener la información del disco');
+            });
+        }
+
+        const handleSendMessage = () => {
+           
+        
+            fetch(`http://127.0.0.1:8000/api/v1/sendMessage/?ip=${ip}&puerto=${port}&libre=${diskInfo.libre}&usado=${diskInfo.utilizado}&total=${diskInfo.total}&pid=${diskInfo.pid}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error al enviar el mensaje a través de socket');
+                }
+                alert('Mensaje enviado con éxito');
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert('Error al enviar el mensaje a través de socket');
+            });
+        }
+        
+
+       
+
+
+        
+          
+
+    return (
+        <div style={centeredDivStyle}>
+            <div style={verticallyCenteredDivStyle}>
+            <h1>Client Page</h1>
+                {diskInfo && (
+                    <div>
+                        <label>Espacio Libre: {diskInfo.libre} GB</label><br />
+                        <label>Espacio Utilizado: {diskInfo.utilizado} GB</label><br />
+                        <label>Espacio Total: {diskInfo.total} GB</label><br/>
+                        <label>Process ID: {diskInfo.pid}</label>
+                    </div>
+                )}
+                <div style={centeredDivStyle}> 
+                    <input 
+                    type="text"
+                    style={textBoxStyle}
+                    value={ip}
+                    onChange={handleIpChange}
+                    />
+                     <input 
+                    type="text"
+                    style={textBoxStyle}
+                    value={port}
+                    onChange={handlePortChange}
+                    />
+                </div>
+                <button
+                style={buttonStyle}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                onClick={handleSpaceCalculation}
+                >
+                    Calcular Espacio
+                </button>
+                
+
+                <button
+                style={buttonStyle}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                onClick={handleSendMessage}
+                >
+                    Enviar
+                </button>
+                
+            </div>
+        </div>
+    );
+}
+
+export default ClientPage;
